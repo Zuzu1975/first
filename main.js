1340e81c375450d5b62e095f4922b28a36e85cbf
Zukhra <muratov01@mail.ru>
2023-04-15 16:17:57 +0600

    первый коммит

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("Hello world!" +5); //строка должна быть в ковычках внутри скобки
+console.log(666); // цыфры могут быть не в ковычках
+console.log("Hello"+" "+" world!"); //"+" "+"  пишется, чтобы два слова не слились
+console.log(666+10); // суммирование
+console.log("666"); //цвет будет черным
+git config --global user.name 
+git config --global user.email muratov01@mail.ru
